60d77bf2b5f45d2fdd3c3c347a07580225c62e02
Karim <98668332+khadni@users.noreply.github.com>
2023-04-04 09:06:52 +0200

    commit

--- contracts/ProjectsFactory.sol
+++ contracts/ProjectsFactory.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.7;
 
 import "./Project.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,42 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1_000,
+          },
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1_000,
+          },
+        },
+      },
+      {
+        version: "0.4.24",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1_000,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     sepolia: {
       url: process.env.SEPOLIA_ALCHEMY_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
+      saveDeployments: true,
     },
   },
   etherscan: {
